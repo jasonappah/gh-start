c4b43adf572c656a264363d1f40908424f57fb9a
Jason Antwi-Appah <hey@jasonaa.me>
2021-08-27 20:49:47 -0500

    feat: set up clap + inquirer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gh-start
-(WIP) A Rust extension for @cli/cli to start new repos from starred template repos
+(WIP) A Rust extension for the GitHub CLI to start new repos from starred template repos
+
+## Developing
+This needs to be built with the nightly version of Rust for now - make sure you have a regular install of Rust, then to set up a nightly build, run `rustup toolchain install nightly`. Then, run `cargo +nightly run` to build and run the CLI once, or `cargo +nightly build` to build a binary.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// (Full example with detailed comments in examples/17_yaml.rs)
+//
+// This example demonstrates clap's building from YAML style of creating arguments which is far
+// more clean, but takes a very small performance hit compared to the other two methods.
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Same as previous examples...
 }
